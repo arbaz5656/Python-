1f7514502496bf94a023718c5074daa5644304be
Shaikh Arbaz Ahmed <139984287+arbaz5656@users.noreply.github.com>
2023-08-24 16:57:32 +0530

    python basic

---
+++ newton1.py
@@ -0,0 +1,145 @@
+# n=["Apple","chappal","gadda","harami"]
+# n[1]="pooma"
+# print(n)
+# print(type(n))
+# # n.remove("Apple")
+# # n.clear()
+#
+# # print(n[0:2])
+# # s= {"a","b"}
+# # print(type(s))
+# x=50
+# y=23
+# z=x+y
+# print(z )
+
+#
+# ahmed=("bhadva","saif","certificate","ghoomaya")
+# # print(ahmed)
+# # e=ahmed.index("ghoomaya")
+# # p"rint(e)
+# o={1:"Arbaz",2:"Ahmed",3:"ishaque"}
+# # print(o)
+# You=input("Enter the key")
+# print(You)
+# # print("result: ",o[You])
+# print("result: ",o.get(You))
+
+
+# NUMBER
+# a=7
+# b=4.5
+# print(a+b)
+# print(type(a))
+# String
+
+"""a='this Is my new line.'
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+"""
+"""
+# b="Shaikh ishaque arbaz ahmed"
+# for x in b:
+#     print(x)
+# print(len(b))
+# print("salim" in b)
+
+# print(b[1:14:2])
+# print(b[-6:-1])
+# print(b[0:])"""
+
+# c="             My new string.         "
+# print(c)
+# print(c.strip())
+"""
+d="My, 2nd ,new string"
+# print(d.replace("new","old"))
+print(d.split(","))"""
+
+"""e="porn"
+f="website"
+# print(e+f)
+# print(e.title())"""
+g=["Arbaz",126,"Aiml",1]
+# print(type(g))
+# g[1]="26"
+# print(g)
+# print(g[0:4:2])
+"""g.append("erhyj")
+print(g)
+g.insert(2,"Dharti park")
+print(g)
+g.remove("erhyj")
+print(g)
+g.clear()
+print(g)
+"""
+# h=["arbaz","arbaz",2,"op"]
+# print(h)
+# i=("arb",)
+# print(type(i))
+# print(i.count("arb"))
+# j={"arbaz","34","arbaz","op"}
+# j.remove("op")
+# print(j)
+# # j.clear()
+# # print(j)
+# j.add("uk")
+# print(j)
+
+# k={"1":"one","2":"two","3":"three","4":"four","f":"five","6":"six"}
+# print(type(k))
+# w=k.get(1)
+# print(k[3])
+# w=k.keys()
+# w=k.values()
+# print(w)
+# i=int(input("Enter the key: "))
+# i=input("Enter the key and you have display values: ")
+# w=k.get(i)
+# print(w)
+# k.clear()
+# print(k)
+# print(type(k))
+# w=k.pop("1")
+# w=k.update({"0":"zero"})
+
+# print(k)
+# l=2
+# if l==1:
+#     print("Wrong")
+# else:
+#     print("right")
+#
+# for i in range(1,10):
+#     print("arbaz    ")
+# i=2
+# while (i<10):
+#     print(i)
+    # i=i+2
+# i=4
+# while(i<10):
+#     print(i)
+    # i=i+3
+
+def func(a):
+    return a
+q=func("arbaz")
+print(q)
+def fun(*bb):
+    print(bb)
+fun("shaikh","sk")
+def f(**q):
+    print("key",["1"])
+v={"1":"arbaz","2":"shaikh"}
+f(**v)
+
+
+
+
+
+
+
+
+
